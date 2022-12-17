54d2d9e75bdcaecc27d3b2c475eca7df007f3a9a
YoussefWilliam <youssef.william997@gmail.com>
2022-12-17 16:01:10 +0200

    [new] learn new way of http

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return 'get messages';
+  }
+
+  @Post()
+  createMessage() {
+    return 'create message';
+  }
+
+  @Get('/:id')
+  getMessage() {
+    return 'get certain message';
+  }
+}
